305a33575c19e0e8f631bd6d089bc374a50ae6c7
Yushan Gao <ysgao@thoughtworks.com>
2018-06-06 14:58:30 +0800

    Init Project

---
+++ src/main/java/com/happy3w/etl/copydb/copydb/CopydbApplication.java
@@ -0,0 +1,12 @@
+package com.happy3w.etl.copydb.copydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopydbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopydbApplication.class, args);
+	}
+}
---
+++ src/test/java/com/happy3w/etl/copydb/copydb/CopydbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.happy3w.etl.copydb.copydb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CopydbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
