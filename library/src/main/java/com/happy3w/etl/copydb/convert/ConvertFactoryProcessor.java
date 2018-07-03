588202688abb6fb0617787e146c777c9aa5a66b2
Yushan Gao <ysgao@thoughtworks.com>
2018-07-03 21:35:37 +0800

    Add commit and license

--- library/src/main/java/com/happy3w/etl/copydb/convert/ConvertFactoryProcessor.java
+++ library/src/main/java/com/happy3w/etl/copydb/convert/ConvertFactoryProcessor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.happy3w.etl.copydb.convert;
 
 import com.happy3w.etl.copydb.convert.primitive.IPrimitiveConverter;
@@ -10,6 +25,10 @@ import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 
+/**
+ * This Processor is used to initialize PrimitiveConverter
+ * @author boroborome
+ */
 @Slf4j
 @Component
 public class ConvertFactoryProcessor implements BeanFactoryPostProcessor, Ordered {
--- library/src/main/java/com/happy3w/etl/copydb/model/type/BigintDataTypeAdapter.java
+++ library/src/main/java/com/happy3w/etl/copydb/model/type/BigintDataTypeAdapter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.happy3w.etl.copydb.model.type;
 
 import com.happy3w.etl.copydb.model.ColumnDefine;
@@ -6,6 +21,10 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Types;
 
+/**
+ * This Adatper is used to adapter Bigint in database
+ * @author boroborome
+ */
 public class BigintDataTypeAdapter extends AbstractDataTypeAdapter {
     public static int Type = Types.BIGINT;
     public BigintDataTypeAdapter() {
